2959314a4e005826a2649bb895f08ccb86ac9a7b
jonnay <jonnay@jonnay.net>
2009-09-21 09:58:19 -0600

    Added some new files and ideas

---
+++ docco/docco.scm
@@ -0,0 +1,8 @@
+;;; Documentation Module
+;; This is to aid in auto-generation of documentation.
+;; The future plan is to allow it to work in two modes
+;; Mode 1 is to document what exists, and stuff it in an org-mode file
+;; Mode 2 is to snarf what exists, and prep it for inclusion into a literate org-mode file.
+;; Mode 1 is first.
+
+
